4dfd74ce2f4d1bbcbd62f7b60badd4c1494840f6
Jtporter9 <jtporter9@gmail.com>
2017-09-27 20:49:02 -0600

    worked on turning call into function

--- server/controllers/productsController.php
+++ server/controllers/productsController.php
@@ -1,19 +1,23 @@
 <?php
-
 require_once('./../resources/variables.php');
 
-//BUILD THE DATABASE CONNECTION WITH host, user, pass, DATABASE
-$dbconnection = mysqli_connect(HOST,USER,PASSWORD,DB_NAME) or die ('connection to DB failed');
-
-// Look up the username & password in the db
-$query = "SELECT * FROM products";
-
-// Results
-$result = mysqli_query($dbconnection, $query) or die ('query failed');
-
-if($result)
+function getAllProducts ($dbconnection) 
 {
-    while($row = mysqli_fetch_array($result)){
+  $dbconnection = mysqli_connect(HOST,USER,PASSWORD,DB_NAME) or die ('connection to DB failed');
+  // Look up the username & password in the db
+  $query = "SELECT * FROM products";
+  // Results
+  $result = mysqli_query($dbconnection, $query) or die ('query failed');
+
+  return $result;
+}
+
+$productsResult = getAllProducts($dbconnection);
+
+if($productsResult)
+{
+  while($row = mysqli_fetch_array($productsResult)) 
+    {
       echo '<div style="margin:10px;border:0.5px solid gray;padding:10px;">';
       echo 'Id #: ' . $row['id'] . '<br>';
       echo 'Name: ' . $row['name'] . '<br>';
--- server/server.php
+++ server/server.php
@@ -1,1 +1,7 @@
 <!-- Routes will be configued here -->
+<?php
+// Dependencies
+include('.\controllers\productsController.php');
+require_once('./resources/variables.php');
+
+?>
