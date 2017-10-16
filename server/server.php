d552e58f28b0344c0d227f6a45de0d0b5e3f281a
Jeremy T. Porter <tanner@lowestmed.com>
2017-10-16 17:56:57 -0600

    server.php ready to be hit

--- server/server.php
+++ server/server.php
@@ -7,6 +7,18 @@ include_once('./controllers/shippingController.php');
 include_once('./controllers/paymentController.php');
 include_once('./controllers/ordersController.php');
 
+if (isset($_GET)) { 
+  $method = $_GET['method'];
+}
+if(isset($_REQUEST['data']))
+{
+  $data = $_REQUEST['data'];
+
+  echo "<code>";
+  print_r($data);
+  echo "</code>";
+}
+
 // ERROR HANDELING
 error_reporting(-1);
 ini_set('display_errors', 'On');
@@ -15,29 +27,87 @@ set_error_handler("var_dump");
 
 $dbconnection = mysqli_connect(HOST,USER,PASSWORD,DB_NAME) or die ('connection to DB failed');
 
-$product = array ("id" => 2, "name" => "Awesome Sauce", "price" => 9.99, "description" => "Awesome.", "quantity" => 99);
-$shipping = array ("id" => 2, "name" => "Tanner", "city" => "Spanish Fork", "state" => "UT", "zip" => 84660, "address" => "1146 S 1180 E");
-$payment = array ("id" => 1, "card_holder" => "Tanner Joey", "card_number" => "2222 2222 2222 2222", "expiration" => "07/19", "cvv" => 123);
-$order = array ("id" => 1, "card_holder" => "Tanner Joey", "card_number" => "2222 2222 2222 2222", "expiration" => "07/19", "cvv" => 123);
-
-echo "<code>";
-print_r($product);
-echo "</code>";
-
-$orderResults = getAllOrders();
-
-// DISPLAY RESULTS
-if($orderResults)
-{
-  while($row = mysqli_fetch_array($orderResults)) 
-    {
-      echo '<div style="margin:10px;border:0.5px solid gray;padding:10px;">';
-      echo 'Id #: ' . $row['id'] . '<br>';
-      echo 'time stamp: ' . $row['time_stamp'] . '<br>';
-      echo 'payment id: $' . $row['payment_id'] . '<br>';
-      echo 'shipping id: ' . $row['shipping_id'] . '<br>';
-      echo '</div>';
-    }
+switch ($method) {
+    //Products
+    case 'getAllProducts':
+        $result = getAllProducts();
+        break;
+    case 'getProductById':
+        $result = getProductById($data);
+        break;
+    case 'updateProduct':
+        $result = updateProduct($data);
+        break;
+    case 'createProduct':
+        $result = createProduct($data);
+        break;
+    case 'deleteProduct':
+        $result = deleteProduct($data);
+        break;
+    //OrderedProducts
+    case 'getAllOrderedProducts':
+        $result = getAllOrderedProducts();
+        break;
+    case 'getOrderedProductById':
+        $result = getAllOrderedProductById($data);
+        break;
+    case 'updateOrderedProduct':
+        $result = updateOrderedProduct($data);
+        break;
+    case 'createOrderedProduct':
+        $result = createOrderedProduct($data);
+        break;
+    case 'deleteOrderedProduct':
+        $result = deleteOrderedProduct($data);
+        break;
+    //Orders
+    case 'getAllOrders':
+        $result = getAllOrders();
+        break;
+    case 'getOrderById':
+        $result = getOrderById($data);
+        break;
+    case 'updateOrder':
+        $result = updateOrder($data);
+        break;
+    case 'createOrder':
+        $result = createOrder($data);
+        break;
+    case 'deleteOrder':
+        $result = deleteOrder($data);
+        break;
+    //Payments
+    case 'getAllPayments':
+        $result = getAllPayments();
+        break;
+    case 'getPaymentById':
+        $result = getPaymentById($data);
+        break;
+    case 'updatePayment':
+        $result = updatePayment($data);
+        break;
+    case 'createPayment':
+        $result = createPayment($data);
+        break;
+    case 'deletePayment':
+        $result = deletePayment($data);
+        break;
+    //Shipping
+    case 'getAllShipping':
+        $result = getAllShipping();
+        break;
+    case 'getShippingById':
+        $result = getShippingById($data);
+        break;
+    case 'updateShipping':
+        $result = updateShipping($data);
+        break;
+    case 'createShipping':
+        $result = createShipping($data);
+        break;
+    case 'deleteShipping':
+        $result = deleteShipping($data);
+        break;
 }
 
 //WE'RE DONE SO HANG UP
