dc228a2f4ebc6f84173f3880dd2b30082f5e9190
jordanmvane <jordanmvane@gmail.com>
2017-10-30 09:57:58 -0600

    Added the admin page

---
+++ server/admin.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<link href="../styling.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8">
+<title>Admin Page</title>
+</head>
+
+<body>
+	<div id="wholething">
+
+		<div id="container">
+
+		    <div class="item_management">
+		        <span>Item Name | <strong>Item Number</strong></span>
+		        <span>Item Description</span>
+		        <form method="post">
+                <input class="button" type="submit" value="+">
+                <input class="button" type="submit" value="-">
+                </form>
+		    </div>
+
+		    <div class="order_management">
+                <span>Order Name</span>
+                <form>
+                <input class="button" type="submit" value="Approve Order">
+                <input class="button" type="submit" value="Disapprove Order">
+                </form>
+                <span>Shipping Info</span>
+                <span>Shipping Status</span>
+            </div>
+
+		</div>
+
+	</div>
+	</div>
+	
+</body>
+</html>
