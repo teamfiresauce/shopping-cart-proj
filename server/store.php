b42c11136deeffbcd190b17603f7823059986286
Jeremy T. Porter <tanner@lowestmed.com>
2017-10-30 19:01:36 -0600

    working on styles

--- server/store.php
+++ server/store.php
@@ -18,52 +18,63 @@ $products = $products_controller->getAllProducts();
 
 ?>
 
-<!doctype html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href=".././styles/styles.css">
-<meta charset="utf-8">
-<title>Store Front</title>
-</head>
+  <!doctype html>
+  <html>
 
-<body>
-	<header>
-        <div>
-            <img class="hot-icon" src=".././images/hot.png" alt="">
-            <big>Team Fire Sauce Store</big>
-        </div>
-        <a href="store.php">Store</a>
-		<a href="cart.php">Cart</a>
+  <head>
+    <link rel="stylesheet" type="text/css" href=".././styles/styles.css">
+    <meta charset="utf-8">
+    <title>Store Front</title>
+  </head>
+
+  <body>
+    <header>
+      <div>
+        <img class="hot-icon" src=".././images/hot.png" alt="">
+        <big>Team Fire Sauce Store</big>
+      </div>
+      <a href="store.php">Store</a>
+      <a href="cart.php">Cart</a>
     </header>
-	<div id="wholething">
-	<?php foreach($products as $product) { ?>
-		<div id="container">
-		<h3><?php echo $product['name'] ?></h3>
-		</a>
-		<br>
-		<br>
-		<div id=photo2>
-		<img src="<?php echo $photoPath ?>" height="275" width="185"/>
-		</div>
+    <div id="wholething">
+      <div class="flex-container">
 
-		<p>
-		<br>
-		<div id=container2>
-		<p><strong>Available Quantity<strong> <?php echo $product['quantity'] ?></p>
-		<p><strong>Description: </strong><?php echo $product['description'] ?></p>
-		<p><strong>Price: </strong><?php echo $product['price'] ?></p>
-		<form action="addToCart.php" method="post">
-			<br>
-			<input name="id" value="<?php echo $product['id'] ?>" type="hidden">
-			<input name="name" value="<?php echo $product['name'] ?>" type="hidden">
-			<input name="price" value="<?php echo $product['price'] ?>" type="hidden">
-			<input name="description" value="<?php echo $product['description'] ?>" type="hidden">
-			<input class="button" type="submit" value="Add to Cart">
-			</form>
-		</div>
-	<?php } ?>
-	</div>
-	</div>
-	
-</body>
-</html>
+        <?php foreach($products as $product) { ?>
+          <div id="container">
+            <h3><?php echo $product['name'] ?></h3>
+            </a>
+            <br>
+            <br>
+            <div id=photo2>
+              <img src="<?php echo $photoPath ?>" height="275" width="185" />
+            </div>
+
+            <p>
+              <br>
+              <div id=container2>
+                <p><strong>Available Quantity<strong> <?php echo $product['quantity'] ?></p>
+    			<p>
+					<strong>Description: </strong>
+                  <?php echo $product['description'] ?>
+                </p>
+                <p><strong>Price: </strong>
+                  <?php echo $product['price'] ?>
+                </p>
+                <form action="addToCart.php" method="post">
+                  <br>
+                  <input name="id" value="<?php echo $product['id'] ?>" type="hidden">
+                  <input name="name" value="<?php echo $product['name'] ?>" type="hidden">
+                  <input name="price" value="<?php echo $product['price'] ?>" type="hidden">
+                  <input name="description" value="<?php echo $product['description'] ?>" type="hidden">
+                  <input class="button" type="submit" value="Add to Cart">
+                </form>
+              </div>
+              <?php } ?>
+          </div>
+
+      </div>
+    </div>
+
+  </body>
+
+  </html>
